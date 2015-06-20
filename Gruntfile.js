4752b54ec80d98eed8a3d1e9a89ff97f33464ced
Ari Porad <ari@porad.com>
2015-06-19 22:28:06 -0700

    Add Browserify [Broken]

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,28 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-
-		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['sass']
-			}
-		}
+        browserify: {
+          files: {
+              'dist/bundle.js': ['assets/vendor/js/**/*.js''assets/js/**/*.js'],
+          },
+          'browserifyOptions': {
+              'debug':true
+          },
+          dist: {
+            'browserifyOptions': {
+              'debug':false
+            }
+          },
+          dev: {},  
+          watch: {
+            watch: true,
+            keepAlive: true
+          }
+        }
 	});
 	require('load-grunt-tasks')(grunt);
-	grunt.registerTask('build', ['watch']);
+    grunt.registerTask('default', ['dist']);
+	grunt.registerTask('dist', ['browserify:dist']);
+	grunt.registerTask('dev', ['browserify:dev']);
+	grunt.registerTask('watch', ['browserify:watch']);
 }
