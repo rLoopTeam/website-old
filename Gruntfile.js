b3062eddda48df8e1d49679b9b34d91c8f45a7f4
Ari Porad <ari@porad.com>
2015-06-20 09:31:54 -0700

    Fixed gruntfile Everything should be working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,114 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-        browserify: {
-          files: {
-              'bundle.js': ['assets/vendor/js/**/*.js''assets/js/**/*.js'],
-          },
-          'browserifyOptions': {
-              'debug':true
-          },
-          dist: {
-            'browserifyOptions': {
-              'debug':false
-            }
-          },
-          dev: {}
-        },
-        jshint: {
-          jshintrc: true
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Copy everything to dist
+    copy: {
+      build: {
+        cwd: 'src',
+        src: ['**'],
+        dest: 'build',
+        expand: true
+      },
+    },
+
+    // Clean /dist
+    clean: {
+      build: {
+        src: ['build']
+      },
+      stylesheets: {
+        src: ['build/**/*.css', '!build/bundle.css']
+      },
+      scripts: {
+        src: ['build/**/*.js', '!build/bundle.js']
+      },
+    },
+
+    // Auto add vendo prefixes
+    build: {
+      expand: true,
+      cwd: 'build',
+      src: ['**/*.css'],
+      dest: 'build'
+    },
+
+    // Minify the CSS
+    cssmin: {
+      build: {
+        files: {
+          'build/bundle.css': ['build/**/*.css']
         }
-	});
-	require('load-grunt-tasks')(grunt);
-    grunt.registerTask('default', ['dist']);
-	grunt.registerTask('dist', ['browserify:dist', 'jshint']);
-	grunt.registerTask('dev', ['browserify:dev', 'jshint']);
-	grunt.registerTask('watch', ['browserify:watch', 'jshint']);
+      }
+    },
+    
+    autoprefixer: {
+      build: {
+        expand: true,
+        cwd: 'build',
+        src: [ '**/*.css' ],
+        dest: 'build'
+      }
+    },
+
+    // Uglify the sourcecode
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */ '
+      },
+      build: {
+        src: 'bundle.js',
+        dest: 'bundle.js'
+      }
+    },
+    
+    browserify: {
+      build: {
+        cwd: 'build',
+        src: 'build/js/index.js',
+        dest: 'build/bundle.js',
+        'browserifyOptions': {
+          'debug':false
+        }
+      }
+    },
+
+    // Watch
+    watch: {
+      stylesheets: {
+        files: ['src/**/*.css', 'src/**/*.scss'],
+        tasks: ['stylesheets']
+      },
+      scripts: {
+        files: 'src/**/*.js',
+        tasks: ['scripts']
+      },
+      copy: {
+        files: ['src/**', '!src/**/*.css', '!src/**/*.scss', '!src/**/*.js'],
+        tasks: ['copy']
+      }
+    },
+
+
+    // Development Server
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: 'build',
+          hostname: '*'
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('stylesheets', 'Compiles the stylesheets.', ['autoprefixer', 'cssmin', 'clean:stylesheets']);
+
+  grunt.registerTask('scripts', 'Compiles the JavaScript files.', ['browserify', 'uglify', 'clean:scripts']);
+
+  grunt.registerTask('build', 'Compiles all of the assets and copies the files to the build directory.', ['clean:build', 'copy', 'stylesheets', 'scripts']);
+  
+  grunt.registerTask('default', 'Watches the project for changes, automatically builds them and runs a server.', [ 'build', 'connect', 'watch' ]);
 }
