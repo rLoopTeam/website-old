92f863b6694fd55daf57c3ebbe73afb0a59c6ae0
Ari Porad <ari@porad.com>
2015-08-11 17:42:28 -0700

    Fix travis.yml, Move deploy to deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -n "${TRAVIS_BRANCH+1}" ]; then
+    BRANCH="$TRAVIS_BRANCH"
+else
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+fi
+
+if [ "$BRANCH" == "master" ]; then
+    npm run deployProd
+elif [ "$BRANCH" == "master-qa" ]; then
+    npm run deployDev
+fi
