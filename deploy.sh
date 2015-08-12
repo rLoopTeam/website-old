b29b0ad95af270c3a4eab7ee3790f4de96b62663
Ari Porad <ari@porad.com>
2015-08-11 21:02:02 -0700

    Refactor deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 set -ev
+
+DEPLOY_HOST=rloop.org
+DEPLOY_USER=dokku
+
+DEV_REPO=dev
+PROD_REPO=prod
+
+npm run build
+mv .gitignore .gitignore.old
+curl www.gitignore.io/api/Node,OSX,Windows,Linux,Vim,Emacs,SublimeText,Textmate,Webstorm > .gitignore
+
 echo "Deploying"
 echo "Checking for branch"
 if [ -n "${TRAVIS_BRANCH+1}" ]; then
@@ -15,9 +25,14 @@ echo $BRANCH
 
 if [ "$BRANCH" == "master" ]; then
     echo "Deploying to Production"
-    npm run deployProd
+    git remote add tmpDeployRemote "$DEPLOY_USER@$DEPLOY_HOST:$PROD_REPO"
 elif [ "$BRANCH" == "master-qa" ]; then
     echo "Deploying to dev"
-    npm run deployDev
+    git remote add tmpDeployRemote "$DEPLOY_USER@$DEPLOY_HOST:$DEV_REPO"
 fi
+
+git subtree push --prefix build tmpDeployRemote master
+cat .gitignore.old > .gitignore
+rm -rf .gitignore.old
+
 echo "Deployed"
