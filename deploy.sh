5b916d87bd5961b8b683ea5f2540ac7332f2bebd
Ari Porad <ari@porad.com>
2015-08-11 17:52:47 -0700

    Fix travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
+echo "Deploying"
 set -ev
+echo "Checking for branch"
 if [ -n "${TRAVIS_BRANCH+1}" ]; then
+    echo "Using travis branch"
     BRANCH="$TRAVIS_BRANCH"
 else
+    echo "Using git branch"
     BRANCH=$(git rev-parse --abbrev-ref HEAD)
 fi
 
 echo $BRANCH
 
 if [ "$BRANCH" == "master" ]; then
+    echo "Deploying to Production"
     npm run deployProd
 elif [ "$BRANCH" == "master-qa" ]; then
+    echo "Deploying to dev"
     npm run deployDev
 fi
+echo "Deployed"
